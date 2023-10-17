f1b25e7f8f965ef2cdad18015ab84e4ad62ae996
irensmile <curkan.ira@gmail.com>
2023-10-17 12:52:46 -0400

    завдання1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+
+let timerId;
+startButton.addEventListener("click", (() => { 
+    //document.body.style.background = getRandomHexColor();
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor()
+    }, 1000)
+}));
+stopButton.addEventListener("click", (() => {
+    clearInterval(timerId);
+}));
